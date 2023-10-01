c659528001c56593893152c90119b540a69cc2d6
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-01 12:43:30 +0700

    function interface dan indexable interface pada typescript

--- tests/interface-readonly.test.ts
+++ tests/interface-readonly.test.ts
@@ -16,5 +16,60 @@ describe("Interface dan Readonly", function () {
       nib: "2329323",
       npwp: "23232321212",
     });
+
+    seller.name = "Maul"; //bisa diubah
+
+    // seller.nib = "2dadasdsads"; //tidak bisa diubah karena menggunakan readyonly
+    console.info(seller);
+
+    console.info(seller);
+    expect(seller).toEqual({
+      id: 1,
+      name: "Maul",
+      nib: "2329323",
+      npwp: "23232321212",
+    });
+  });
+
+  it("should support function interface", function () {
+    interface addFunction {
+      (value1: number, value2: number): number;
+    }
+
+    const add: addFunction = (value1: number, value2: number): number => {
+      return value1 + value2;
+    };
+
+    expect(add(5, 10)).toBe(15);
+  });
+
+  it("should support indexable interface use number ", () => {
+    interface numberArray {
+      [index: number]: string; //bisa juga valuenya number dll
+    }
+
+    const name: numberArray = ["Maul", "Fachri", "Dani", "Bintang"];
+    console.info(name);
+    console.info(name[0]);
+    console.info(name[1]);
+    console.info(name[2]);
+    console.info(name[3]);
+  });
+
+  it("should support indexable interface use string || not number", function () {
+    interface stringArray {
+      [index: string]: string | number;
+    }
+
+    const dictionary: stringArray = {
+      name: "Maulana",
+      address: "Jakarta",
+      age: 21,
+    };
+
+    console.info(dictionary);
+    console.info(dictionary.name); // bisa juga gunakan ["name"]
+    console.info(dictionary.address); // bisa juga gunakan ["address"]
+    console.info(dictionary.age); // bisa juga gunakan ["age"]
   });
 });
